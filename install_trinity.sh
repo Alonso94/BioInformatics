8710f5773eab104a017bd5dad054ea4fb066e2e7
Ali Younes <ay02-5-1994@hotmail.com>
2021-03-08 19:51:35 +0300

    Update install_trinity.sh

--- install_trinity.sh
+++ install_trinity.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+sudo apt install -y cmake samtools jellyfish bowtie2 salmon libzma-dev autoconf libproxy-dev libz-dev
 git clone https://github.com/trinityrnaseq/trinityrnaseq.git
 cd trinityrnaseq
 git checkout tags/v2.11.0
@@ -7,6 +7,5 @@ git submodule update --init --recursive
 make
 make plugins
 make install
-sudo apt install -y cmake samtools jellyfish bowtie2 salmon libzma-dev autoconf libproxy-dev libz-dev
 echo 'export TRINITY_HOME=/usr/local/bin/trinityrnaseq' >> ~/.bashrc
 echo 'alias Trinity=$TRINITY_HOME/Trinity' >> ~/.bashrc
